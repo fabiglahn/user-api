6555326bc7e784df4939f2c676d164584e4fb893
“[fabiglahn] <[fabian-glahn@hotmail.de]>
2021-10-26 13:59:55 +0200

    add routes and 404

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,21 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+const users = ['Alice', 'Manu', 'Fabi'];
+
+app.get('/api/users/:name', (request, response) => {
+  const isKnownName = users.includes(request.params.name);
+  if (isKnownName) {
+    response.send(request.params.name);
+  } else {
+    response.status(404).send('Sorry! Name is unknown! :(');
+  }
+});
+
+app.get('/api/users', (_request, response) => {
+  response.send(users);
+});
+
 app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
