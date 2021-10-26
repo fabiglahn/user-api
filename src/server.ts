127830e5f9af607e8a33fe61e4b0ba1db83bc70c
“[fabiglahn] <[fabian-glahn@hotmail.de]>
2021-10-26 17:01:02 +0200

    prepare POST user

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,24 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 const users = ['Alice', 'Manu', 'Fabi'];
+
+app.post('/api/users', (request, response) => {
+  response.send(request.body.name);
+});
+
+app.delete('/api/users/:name', (request, response) => {
+  const index = users.indexOf(request.params.name);
+  if (index === -1) {
+    response.status(404).send('Sorry! Name is unknown! :(');
+    return;
+  }
+
+  users.splice(index, 1);
+  response.send('deleted');
+});
 
 app.get('/api/users/:name', (request, response) => {
   const isKnownName = users.includes(request.params.name);
